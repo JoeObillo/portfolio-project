0d2d151cf72a0f1c136ea285e8d0e559b907fa77
jia <obillojoewen@gmail.com>
2024-08-17 16:54:38 +0800

    changed colors, changed projects page layout, added skills | will edit mobile layout someday

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var mainListDiv = document.getElementById("mainListDiv"),
-    mediaButton = document.getElementById("mediaButton");
+  mediaButton = document.getElementById("mediaButton");
 
 mediaButton.onclick = function () {
-    
-    "use strict";
-    
-    mainListDiv.classList.toggle("show_list");
-    mediaButton.classList.toggle("active");
-    
+  "use strict";
+
+  mainListDiv.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
 };
+
+function toggleText(skills, skills2) {
+  var text = document.getElementById(skills);
+  var text2 = document.getElementById(skills2);
+  if (text2.style.display === "none") {
+    text.style.display = "none";
+    text2.style.display = "block";
+  } else {
+    text.style.display = "block";
+    text2.style.display = "none";
+  }
+}
